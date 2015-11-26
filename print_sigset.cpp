ed852e14d6216b08f5248b39cd9503713f7f7b95
tutu <tutu@localhost.localdomain>
2015-11-26 05:06:20 -0500

    source_code_part

---
+++ print_sigset.cpp
@@ -0,0 +1,18 @@
+/*
+ * 	This file is used to print a process's sigset
+ *
+ * 	NOTE: All of the following functions employ fprintf() , which is not async-signal-safe .
+ * 	As such , these functions are also not async-signal-safe(i.e., beware of indiscriminately 
+ * 	calling then from signal handlers).
+ *
+ */
+
+#define 	_GNU_SOURCE
+#include "apue_signal.h"
+
+int main (int argc , char *argv[]) {
+
+
+	return 0 ;
+}
+
